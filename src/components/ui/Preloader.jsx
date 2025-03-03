44cd7fc045972edd0703f184928d0e644eec6f25
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2025-03-03 23:26:42 +0000

    feat: implement preloader animation for initial app load

---
+++ src/components/ui/Preloader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Intro from "../../assets/Intro/JobNest.mp4"
+function Preloader({ className }) {
+    return (
+        <div className="fixed inset-0 flex items-center justify-center bg-black z-50">
+            <video autoPlay muted className="w-full h-full object-contain">
+                <source src={Intro} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+        </div>
+    );
+}
+
+export default Preloader;
