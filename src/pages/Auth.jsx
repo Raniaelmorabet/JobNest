b5b6c23405034a17fe4bf95eb088b46ab17cd926
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2025-03-12 01:54:41 +0000

    feat: add Auth.tsx component for authentication UI enhancements

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,11 +1,9 @@
 import AuthUI from "@/components/Auth-ui"
-// import MobileAuthUI from "@/components/mobile-auth-ui"
 
 export default function Auth() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-white">
       <AuthUI />
-      {/* <MobileAuthUI /> */}
     </main>
   )
 }
