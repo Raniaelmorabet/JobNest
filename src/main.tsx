12ffd9a2b54d93f5924fa6cd9ff7f2dfce03a0e5
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2025-03-12 02:41:38 +0000

    refactor: update main.jsx to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
