146d15f8fcc0a9419cd774e89178a11e43815061
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2025-03-12 01:57:37 +0000

    chore: update main.jsx for compatibility with updated components

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
-import { AuthProvider } from "./context/AuthContext.jsx";
-// import 'flowbite';
+import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
