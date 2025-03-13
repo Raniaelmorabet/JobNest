c4dc27b9b6b841475d6e388f63704f05651b8c8b
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2025-03-13 01:42:28 +0000

    feat: added admin protector to my dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -11,73 +11,85 @@ import JobDetailsEmployer from "./components/JobDetailsEmployer";
 import Error from "@/components/Error";
 
 function App(): JSX.Element {
-  const location = useLocation();
-  const { token, user } = useAuth();
+    const location = useLocation();
+    const { token, user } = useAuth();
 
-  // Scroll to top on route change
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [location.pathname]);
+    // Scroll to top on route change
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location.pathname]);
 
-  // ProtectedRoute component
-  const ProtectedRoute = ({ children }: { children: ReactNode }): JSX.Element => {
-    if (!token || !user) {
-      return <Navigate to="/auth" replace />;
-    }
-    return <>{children}</>;
-  };
+    // ProtectedRoute component
+    const ProtectedRoute = ({
+                                children,
+                            }: {
+        children: ReactNode;
+    }): JSX.Element => {
+        if (!token || !user) {
+            return <Navigate to="/auth" replace />;
+        }
+        return <>{children}</>;
+    };
 
-  const ProtectedAdmin = ({ children }: { children: ReactNode }): JSX.Element => {
-    if (!user || JSON.parse(user).role === "employer") {
-      return <Navigate to="/dashboard" replace />;
-    }
-    return <>{children}</>;
-  };
+    const ProtectedAdmin = ({
+                                children,
+                            }: {
+        children: ReactNode;
+    }): JSX.Element => {
+        if (!user || JSON.parse(user).role != "employer") {
+            return <Navigate to="/" replace />;
+        }
+        return <>{children}</>;
+    };
 
-  return (
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/auth" element={<Auth />} />
-            <Route path="*" element={<Error />} />
-            <Route
-                path="/"
-                element={
-                  <ProtectedAdmin>
-                    <Home />
-                  </ProtectedAdmin>
-                }
-            />
-            <Route
-                path="/jobs/:id"
-                element={
-                  <ProtectedRoute>
-                    <JobDetail />
-                  </ProtectedRoute>
-                }
-            />
-            <Route
-                path="/dashboard"
-                element={
-                  <ProtectedRoute>
-                    <DashboardPage />
-                  </ProtectedRoute>
-                }
-            />
-            <Route
-                path="/dashboard/jobs/:id"
-                element={
-                  <ProtectedRoute>
-                    <JobDetailsEmployer />
-                  </ProtectedRoute>
-                }
-            />
-          </Routes>
-        </main>
-        {location.pathname === "/auth" ? "" : <Footer />}
-      </div>
-  );
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-grow">
+                <Routes>
+                    <Route path="/auth" element={<Auth />} />
+                    <Route path="*" element={<Error />} />
+                    <Route
+                        path="/"
+                        element={
+                            <ProtectedRoute>
+                                <Home />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route
+                        path="/jobs/:id"
+                        element={
+                            <ProtectedRoute>
+                                <JobDetail />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route
+                        path="/dashboard"
+                        element={
+                            <ProtectedRoute>
+                                <ProtectedAdmin>
+                                    <DashboardPage />
+                                </ProtectedAdmin>
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route
+                        path="/dashboard/jobs/:id"
+                        element={
+                            <ProtectedRoute>
+                                <ProtectedAdmin>
+                                    <JobDetailsEmployer />
+                                </ProtectedAdmin>
+                            </ProtectedRoute>
+                        }
+                    />
+                </Routes>
+            </main>
+            {location.pathname === "/auth" ? "" : <Footer />}
+        </div>
+    );
 }
 
 export default App;
